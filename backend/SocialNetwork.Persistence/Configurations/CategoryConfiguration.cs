edf451aac2523aafc8d8b5a9a2350421c90acaa8
Akhmetshin Tagir <tagir.ahmetshin@a2soft.ru>
2025-09-28 23:24:55 +0300

    обновил миграции и добавил кронджоб

--- backend/SocialNetwork.Persistence/Configurations/CategoryConfiguration.cs
+++ backend/SocialNetwork.Persistence/Configurations/CategoryConfiguration.cs
@@ -18,31 +18,31 @@ namespace FilmMatch.Persistence.Configurations
                 {
                     Id = Guid.Parse("d0bfe76e-0f12-4fcd-94aa-3be4f90d79e1"),
                     Name = "Фантастика",
-                    ImageUrl = "http://localhost:5210/images/category/fantasy.png"
+                    ImageUrl = "https://atomvestnik.ru/wp-content/uploads/2022/08/%D0%9F%D1%80%D0%BE%D1%81%D1%82%D0%BE-%D1%84%D0%B0%D0%BD%D1%82%D0%B0%D1%81%D1%82%D0%B8%D0%BA%D0%B0-%D0%93%D0%9B%D0%90%D0%92%D0%9D%D0%90%D0%AF.jpg"
                 },
                 new Category
                 {
                     Id = Guid.Parse("31f80f2a-9426-41e2-93f7-7f12180722a1"),
                     Name = "Триллер",
-                    ImageUrl = "http://localhost:5210/images/category/triller.jpg"
+                    ImageUrl = "https://icdn.lenta.ru/images/2024/06/19/11/20240619112717688/preview_a517b64420fe27aa683b31e8a306868d.jpg"
                 },
                 new Category
                 {
                     Id = Guid.Parse("5dbd7a97-f0a1-4f4e-91c7-244cbab17eec"),
                     Name = "Комедия",
-                    ImageUrl = "http://localhost:5210/images/category/comedy.jpg"
+                    ImageUrl = "https://images.iptv.rt.ru/images/cemmbqbir4ssk1206l40.jpg"
                 },
                 new Category
                 {
                     Id = Guid.Parse("4b4974f1-8ea2-43f1-998f-d3a1cfb9d1c3"),
                     Name = "Драма",
-                    ImageUrl = "http://localhost:5210/images/category/drama.png"
+                    ImageUrl = "https://www.kinonews.ru/insimgs/imgzhanr9.webp"
                 },
                 new Category
                 {
                     Id = Guid.Parse("0b27972c-b3df-4ae4-9138-2e90c749d139"),
                     Name = "Боевик",
-                    ImageUrl = "http://localhost:5210/images/category/action.png"
+                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/040a010c-660d-4185-946e-2cd812852ee2/600x900"
                 }
             );
         }
--- backend/SocialNetwork.Persistence/Configurations/FilmConfiguration.cs
+++ backend/SocialNetwork.Persistence/Configurations/FilmConfiguration.cs
@@ -15,7 +15,7 @@ namespace FilmMatch.Persistence.Configurations
                     Id = Guid.Parse("a1e7b5fa-34df-4bc4-902e-cdfb10dcf001"),
                     Title = "Интерстеллар",
                     ReleaseDate = DateTime.SpecifyKind(new DateTime(2014, 11, 7), DateTimeKind.Utc),
-                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/4771096/2a0000017e39d1cfcb48b5f4fe5a81e8b9f4/1920x",
+                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/430042eb-ee69-4818-aed0-a312400a26bf/600x900",
                     LongDescription = "Эпический научно-фантастический фильм о путешествии через червоточину.",
                     ShortDescription = "Путешествие сквозь космос и время.",
                     CategoryId = Guid.Parse("d0bfe76e-0f12-4fcd-94aa-3be4f90d79e1")
@@ -25,7 +25,7 @@ namespace FilmMatch.Persistence.Configurations
                     Id = Guid.Parse("8cb06d19-68aa-4ce3-a1a6-76e48d9f4d55"),
                     Title = "Начало",
                     ReleaseDate = DateTime.SpecifyKind(new DateTime(2010, 7, 16), DateTimeKind.Utc),
-                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/4771096/2a0000017e39d1cfcb48b5f4fe5a81e8b9f4/1920x",
+                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/8ab9a119-dd74-44f0-baec-0629797483d7/600x900",
                     LongDescription = "Триллер о проникновении в сны и манипуляции сознанием.",
                     ShortDescription = "Погружение в мир сновидений.",
                     CategoryId = Guid.Parse("31f80f2a-9426-41e2-93f7-7f12180722a1")
@@ -35,7 +35,7 @@ namespace FilmMatch.Persistence.Configurations
                     Id = Guid.Parse("afde4eeb-c0b7-404f-aad0-0d188fe9a921"),
                     Title = "Матрица",
                     ReleaseDate = DateTime.SpecifyKind(new DateTime(1999, 3, 31), DateTimeKind.Utc),
-                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/4771096/2a0000017e39d1cfcb48b5f4fe5a81e8b9f4/1920x",
+                    ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/a53d1c75-4d1a-4c86-a936-65b2a724345c/600x900",
                     LongDescription = "Культовый фильм о виртуальной реальности и борьбе за свободу.",
                     ShortDescription = "Виртуальный мир и реальность.",
                     CategoryId = Guid.Parse("d0bfe76e-0f12-4fcd-94aa-3be4f90d79e1")
